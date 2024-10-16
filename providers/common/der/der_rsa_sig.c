ce93a26106d024dbd3050629ed2c6ac259866e7d
cyf <505224730@qq.com>
2024-10-16 17:33:12 +0800

    rm sm4_rsa

--- providers/common/der/der_rsa_sig.c
+++ providers/common/der/der_rsa_sig.c
@@ -36,7 +36,6 @@ int ossl_DER_w_algorithmIdentifier_MDWithRSAEncryption(WPACKET *pkt, int tag,
 
     switch (mdnid) {
 #ifndef FIPS_MODULE
-        MD_with_RSA_CASE(md4, precompiled);
         MD_with_RSA_CASE(md5, precompiled);
 #endif
         MD_with_RSA_CASE(sha1, precompiled);
